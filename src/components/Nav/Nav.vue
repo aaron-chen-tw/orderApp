a6917f73f9dd2295c7fcd7ba64de69c7a1f0f8de
aaron-chen-tw <64724443+aaron-chen-tw@users.noreply.github.com>
2021-02-08 11:16:32 +0800

    Add files via upload

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,64 +1,55 @@
 <template>
-	<div class="nav">
-		<!-- 使用 router-link 组件来导航. -->
-		<!-- 通过传入 `to` 属性指定链接. -->
-		<!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-		<router-link to="/goods" class="nav-item">
-			点菜
-			<i class="line"></i>
-		</router-link>
-		<router-link to="/ratings" class="nav-item">
-			评价({{commentNum}})
-			<i class="line"></i>
-		</router-link>
-		<router-link to="/seller" class="nav-item">
-			商家
-			<i class="line"></i>
-		</router-link>
-	</div>
+  <div class="nav">
+    <!-- <div class="nav-item goods">點餐</div>
+    <div class="nav-item ratings">評分</div>
+    <div class="nav-item seller">店家</div>-->
+    <router-link to="/goods" class="nav-item">
+      點餐
+      <i class="line"></i>
+    </router-link>
+    <router-link to="/ratings" class="nav-item">
+      評分
+      <i class="line"></i>
+    </router-link>
+    <router-link to="/seller" class="nav-item">
+      店家
+      <i class="line"></i>
+    </router-link>
+  </div>
 </template>
 
 <script>
-	export default {
-		props: {
-			commentNum: {
-				type: Number,
-				default: 0
-			}
-		}
-	}
+export default {};
 </script>
 
 <style>
-	.nav {
-		display: flex;
-		width: 100%;
-		height: 40px;
-		line-height: 40px;
-		border-bottom: 1px solid #E4E4E4;
-	}
-	
-	.nav .nav-item {
-		flex: 1;
-		text-align: center;
-		font-size: 13px;
-		text-decoration: none;
-		color: #666666;
-		position: relative;
-	}
-	
-	/* 选中后的样式*/
-	.nav .active{
-		color: #FFBB22;
-	}
-	.nav .active .line{
-		width: 20px;
-		height: 2px;
-		background: #FFBB22;
-		display: inline-block;
-		position: absolute;
-		left: 50%;
-		bottom: 0;
-		margin-left: -10px;
-	}
+.nav {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid #e4e4e4;
+}
+.nav .nav-item {
+  flex: 1;
+  text-align: center;
+  font-size: 13px;
+  text-decoration: none;
+  color: #666666;
+  position: relative;
+}
+
+.nav .active {
+  color: #ffbb22;
+}
+.nav .active .line {
+  width: 20px;
+  height: 2px;
+  background: #ffbb22;
+  display: inline-block;
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  margin-left: -10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import Axios from 'Axios'
+// import VueRouter from 'vue-router'
+// import Axios from 'Axios'
 
-// 1、导入组件
+// // 1、导入组件
 import Goods from "components/Goods/Goods"
 import Ratings from "components/Ratings/Ratings"
 import Seller from "components/Seller/Seller"
+// import Goods from "components/Goods/Goods"
+// import Ratings from "components/Ratings/Ratings"
+// import Seller from "components/Seller/Seller"
 
 
 Vue.config.productionTip = false
@@ -17,42 +22,69 @@ Vue.config.productionTip = false
 Vue.use(VueRouter);
 // 如果在其他组件中使用axios命令，需要改写为Vue原型属性
 Vue.prototype.$axios = Axios;
+// Vue.prototype.$axios = Axios;
 
 
-// 2、定义路由
+// // 2、定义路由
+
 const routes = [
 	{
 		path: '/',
 		// 重定向
-		redirect: '/goods'	// '/' ==> '/goods'
+		redirect: '/goods'
 	},
 	{
-		path:'/goods',
+		path: '/goods',
 		component: Goods
 	},
 	{
-		path:'/ratings',
+		path: '/ratings',
 		component: Ratings
 	},
 	{
-		path:'/seller',
+		path: '/seller',
 		component: Seller
-	}
+	},
 ];
 
-// 3. 创建 router 实例
 const router = new VueRouter({
 	routes,
-	// 选中后的类名 (默认值是router-link-active)
 	linkActiveClass: 'active'
 })
+
+// const routes = [
+// 	{
+// 		path: '/',
+// 		// 重定向
+// 		redirect: '/goods'	// '/' ==> '/goods'
+// 	},
+// 	{
+// 		path: '/goods',
+// 		component: Goods
+// 	},
+// 	{
+// 		path: '/ratings',
+// 		component: Ratings
+// 	},
+// 	{
+// 		path: '/seller',
+// 		component: Seller
+// 	}
+// ];
+
+// // 3. 创建 router 实例
+// const router = new VueRouter({
+// 	routes,
+// 	// 选中后的类名 (默认值是router-link-active)
+// 	linkActiveClass: 'active'
+// })
 
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',	
-  template: '<App/>',
-  components: { App },
-  // 4. 创建和挂载根实例
-  router
+	el: '#app',
+	template: '<App/>',
+	components: { App },
+	// 4. 创建和挂载根实例
+	router
 })
